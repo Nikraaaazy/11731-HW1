d19107691fab42af1137abefcfa7a331e16296c5
Nick Lu <Nikraaaazy@me.com>
2019-09-28 17:05:44 -0400

    AWS commit

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch
 
 class NMT(nn.Module):
 
-    def __init__(self, embed_size, hidden_size, vocab, dropout_rate=0.2, num_layers = 2):
+    def __init__(self, embed_size, hidden_size, vocab, dropout_rate=0.2, num_layers = 1):
         super(NMT, self).__init__()
         self.embed_size = embed_size
         self.hidden_size = hidden_size
@@ -16,9 +16,9 @@ class NMT(nn.Module):
         self.source_embedding = nn.Embedding(len(vocab.src), embed_size, padding_idx=0)
         self.target_embedding = nn.Embedding(len(vocab.tgt), embed_size, padding_idx=0)
         self.encoder = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
-        self.decoder = nn.LSTM(input_size=embed_size, hidden_size=2*hidden_size, num_layers=num_layers)
-        # self.affine = nn.Linear(hidden_size*2, hidden_size)
-        self.linear = nn.Linear(2*hidden_size, len(vocab.tgt))
+        self.decoder = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers)
+        self.affine = nn.Linear(hidden_size*2, hidden_size)
+        self.linear = nn.Linear(hidden_size, len(vocab.tgt))
 
     def forward(self, src_sents: Tensor, tgt_sents: Tensor) -> Tensor:
         """
@@ -33,8 +33,8 @@ class NMT(nn.Module):
         _, B, V = h.size()
         h = h.reshape(self.num_layers, 2, B, V).permute(0, 2, 3, 1).reshape(self.num_layers, B, -1)
         c = c.reshape(self.num_layers, 2, B, V).permute(0, 2, 3, 1).reshape(self.num_layers, B, -1)
-        # h = self.affine(h)
-        # c = self.affine(c)
+        h = self.affine(h)
+        c = self.affine(c)
         tgt_sents = self.target_embedding(tgt_sents)
         output, _ = self.decoder(tgt_sents, (h, c))
         output = self.linear(output)
