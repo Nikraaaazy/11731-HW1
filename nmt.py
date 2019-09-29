0adaca0fa6829fd195ed022e830167796c7a4348
Nick Lu <Nikraaaazy@me.com>
2019-09-28 21:45:53 -0400

    AWS commit

--- nmt.py
+++ nmt.py
@@ -103,7 +103,7 @@ def train(args: Dict[str, str]):
             model = nn.DataParallel(model)
         model.cuda()
     criterion = torch.nn.CrossEntropyLoss(ignore_index=0)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.003, weight_decay=0.1)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.003, weight_decay=0.1)
     best_ppl = float("inf")
     best_model = None
 
