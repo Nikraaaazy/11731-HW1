48f7d79d971774b3394557b7ed8ca28d23d3f18a
Nick Lu <Nikraaaazy@me.com>
2019-09-28 22:17:32 -0400

    AWS commit

--- model.py
+++ model.py
@@ -20,10 +20,9 @@ class NMT(nn.Module):
         self.num_layers = num_layers
         self.source_embedding = nn.Embedding(len(vocab.src), embed_size, padding_idx=0)
         self.target_embedding = nn.Embedding(len(vocab.tgt), embed_size, padding_idx=0)
-        self.encoder = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
-        self.decoder = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers)
-        self.affine = nn.Linear(hidden_size*2, hidden_size)
-        self.linear = nn.Linear(hidden_size, len(vocab.tgt))
+        self.encoder = script_lnlstm(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
+        self.decoder = script_lnlstm(input_size=embed_size, hidden_size=2*hidden_size, num_layers=num_layers)
+        self.linear = nn.Linear(2*hidden_size, len(vocab.tgt))
         self.register_buffer("h_0", torch.zeros(hidden_size))
         self.register_buffer("c_0", torch.zeros(hidden_size))
 
@@ -33,18 +32,17 @@ class NMT(nn.Module):
         :param tgt_sents: (T * B) Padded target sequence, masking will be handled by masking
         :return: logits (T * B * target_vocab_size)
         """
-        source_length = (src_sents != 0).sum(dim=0)
+        # source_length = (src_sents != 0).sum(dim=0)
         src_sents = self.source_embedding(src_sents)
-        src_sents = pack_padded_sequence(src_sents, source_length)
-        _, (h, c) = self.encoder(src_sents)
-        _, B, V = h.size()
-        h = h.reshape(self.num_layers, 2, B, V).permute(0, 2, 1, 3).reshape(self.num_layers, B, -1)
-        c = c.reshape(self.num_layers, 2, B, V).permute(0, 2, 1, 3).reshape(self.num_layers, B, -1)
-        h = self.affine(h)
-        c = self.affine(c)
+        # src_sents = pack_padded_sequence(src_sents, source_length)
+        h_and_c = [[(self.h_0, self.c_0)] * 2] * self.num_layers
+        _, h_and_c = self.encoder(src_sents, h_and_c)
+        # _, B, V = h.size()
+        # h = h.reshape(self.num_layers, 2, B, V).permute(0, 2, 1, 3).reshape(self.num_layers, B, -1)
+        # c = c.reshape(self.num_layers, 2, B, V).permute(0, 2, 1, 3).reshape(self.num_layers, B, -1)
+        h_and_c = [(torch.cat([temp[0][0], temp[1][0]]), torch.cat([temp[0][1], temp[1][1]])) for temp in h_and_c]
         tgt_sents = self.target_embedding(tgt_sents)
-        output, _ = self.decoder(tgt_sents, (h, c))
-        # output, _ = self.decoder(tgt_sents)
+        output, _ = self.decoder(tgt_sents, h_and_c)
         output = self.linear(output)
         return output
 
