5096546173f463faad9345ebe5c801dba0f78f95
Nick Lu <Nikraaaazy@me.com>
2019-09-29 17:11:34 -0400

    Beam search init

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,37 +12,37 @@ work_dir="work_dir"
 mkdir -p ${work_dir}
 echo save results to ${work_dir}
 
-#python nmt.py \
-#    train \
-#    --vocab ${vocab} \
-#    --train-src ${train_src} \
-#    --train-tgt ${train_tgt} \
-#    --dev-src ${dev_src} \
-#    --dev-tgt ${dev_tgt} \
-#    --save-to ${work_dir}/model.bin \
-#    --valid-niter 2400 \
-#    --batch-size 64 \
-#    --hidden-size 256 \
-#    --embed-size 256 \
-#    --uniform-init 0.1 \
-#    --dropout 0.2 \
-#    --clip-grad 5.0 \
-#    --lr-decay 0.5 \
-#    --lr 0.0003 \
-#    --cuda \
-
 python nmt.py \
-    decode \
-    --cuda \
-    --beam-size 5 \
-    --max-decoding-time-step 100 \
+    train \
+    --vocab ${vocab} \
+    --train-src ${train_src} \
+    --train-tgt ${train_tgt} \
+    --dev-src ${dev_src} \
+    --dev-tgt ${dev_tgt} \
+    --save-to ${work_dir}/model.bin \
+    --valid-niter 2400 \
+    --batch-size 64 \
     --hidden-size 256 \
     --embed-size 256 \
-    --vocab ${vocab} \
     --uniform-init 0.1 \
     --dropout 0.2 \
-    ${work_dir}/model.bin \
-    ${test_src} \
-    ${work_dir}/decode.txt
+    --clip-grad 5.0 \
+    --lr-decay 0.5 \
+    --lr 0.0003 \
+    --cuda \
 
-perl multi-bleu.perl ${test_tgt} < ${work_dir}/decode.txt
+#python nmt.py \
+#    decode \
+#    --cuda \
+#    --beam-size 5 \
+#    --max-decoding-time-step 100 \
+#    --hidden-size 256 \
+#    --embed-size 256 \
+#    --vocab ${vocab} \
+#    --uniform-init 0.1 \
+#    --dropout 0.2 \
+#    ${work_dir}/model.bin \
+#    ${test_src} \
+#    ${work_dir}/decode.txt
+#
+#perl multi-bleu.perl ${test_tgt} < ${work_dir}/decode.txt
